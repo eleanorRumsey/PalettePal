4981d459b8ac7a15ccea562cb4e6162d94235e5d
Mikeli Habash <mhaba076@uottawa.ca>
2019-07-15 15:23:04 -0400

    create new account page and navigation

--- src/app/color-picker/color-picker.module.ts
+++ src/app/color-picker/color-picker.module.ts
@@ -2,6 +2,7 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule } from 'nativescript-angular/common';
 
 
+
 @NgModule({
   declarations: [],
   imports: [
